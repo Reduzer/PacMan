b28bfe150b82d08fee371c0fe8226e429f69e183
Marvin01012 <86465358+Marvin01012@users.noreply.github.com>
2021-07-01 14:10:49 +0200

    Add files via upload

---
+++ C-PacMan-main/Pac-Man/Window2.xaml.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pac_Man
+{
+    /// <summary>
+    /// Interaktionslogik für Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        #region Anzeige_Sachen
+        int lebenanzahl = 3;
+        int punkteanzahl = 0;
+        #endregion
+
+        #region Bewegungen
+
+        //Wenn die Bewegung möglich ist
+        public bool hoch, runter, rechts, links;
+        public bool hochnicht, runternicht, rechtsnicht, linksnicht = false;
+
+        public double geschwindigkeit = 5;
+        //double geistergeschwindigkeit = 3;
+        public bool intersection;
+
+        #endregion
+
+        public Window2()
+        {
+            InitializeComponent();
+            #region
+            Leben.Content = "Deine Leben: " + lebenanzahl;
+            Punkte.Content = "Deine Punkte: " + punkteanzahl;
+            #endregion
+
+            Spielfeld.Focus();
+
+        }
+
+
+        private void aktivesspiel()
+        {
+
+            #region Wand oder keine Wand
+            //Canvas.Left und Canvas.Top in XAML code vorhanden evtl in C# anwendbar?
+
+            /*
+            Ja ist anwendbar aber muss mit der groeße des Fensters abgestimmt werden,
+            damit der Spieler nicht vom Bildschirm runter kann
+            */
+
+            //Möglich über Rect.IntersectWith(Rect)
+
+
+
+            /*
+            int array1 = new int();
+            array1 = {
+            "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1",
+ 
+            };
+            */
+
+            int[,] map = new int[31, 28]
+            {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 0, 1, 2, 1, 1, 2, 1, 0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 3, 3, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1},
+                {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
+                {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            };
+             
+            #endregion
+
+            #region Bewegung
+            if (links == true && linksnicht == false && Canvas.GetLeft(spieler) > 0)
+            {
+                Canvas.SetLeft(spieler, (Canvas.GetLeft(spieler) - geschwindigkeit));
+            }
+
+            if(rechts == true && rechtsnicht == false && Canvas.GetLeft(spieler) < 670)
+            {
+                Canvas.SetLeft(spieler, (Canvas.GetLeft(spieler) + geschwindigkeit));
+            }
+
+            if(hoch == true && hochnicht == false&& Canvas.GetTop(spieler) > 0)
+            {
+                Canvas.SetTop(spieler, (Canvas.GetTop(spieler) - geschwindigkeit));
+            }
+
+            if(runter == true && runternicht == false && Canvas.GetTop(spieler) < 750)
+            {
+                Canvas.SetTop(spieler, (Canvas.GetTop(spieler) + geschwindigkeit));
+            }
+            #endregion
+
+        }
+
+        public void MapZeichnen()
+        {
+            int[,] map = new int[31, 28]
+            {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 0, 1, 2, 1, 1, 2, 1, 0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 3, 3, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 3, 3, 3, 3, 3, 3, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1},
+                {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
+                {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
+                {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+                {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            };
+            for (int i = 0; i < 28; i++)
+            {
+                for(int j = 0; j < 31; j++)
+                {
+                    Button mapTile = new Button();
+                    switch (map[i,j])
+                    {
+                        case 0:
+                            Grid.SetColumn(mapTile, j);
+                            Grid.SetRow(mapTile, i);
+                            mapTile.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                            break;
+                        case 1:
+                            Grid.SetColumn(mapTile, j);
+                            Grid.SetRow(mapTile, i);
+                            mapTile.Background = new SolidColorBrush(Color.FromRgb(0, 0, 100));
+                            break;
+                        case 2:
+                            Grid.SetColumn(mapTile, j);
+                            Grid.SetRow(mapTile, i);
+                            mapTile.Background = new SolidColorBrush(Color.FromRgb(255, 255, 50));
+                            break;
+                        case 3:
+                            Grid.SetColumn(mapTile, j);
+                            Grid.SetRow(mapTile, i);
+                            mapTile.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                            break;
+                        default:
+                            Grid.SetColumn(mapTile, j);
+                            Grid.SetRow(mapTile, i);
+                            mapTile.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void drehung(Object sender, KeyEventArgs e)
+        {
+            
+            #region Bewegung Selbst
+
+            //Evtl möglich über if
+            /*
+            Ja ist möglich die Abfrage für das KeyEventArgs müss dem Entsprechenden den Key angepasst werden
+            Die Booleans die nicht benötigt werden, werden auf false gesetzt um fehler zu vermeiden.
+
+            P.S:
+            Der Spieler kann trotz das die Booleans auf false gesetzt werden gleichzeitig z.B. rechts und hoch drücken
+            Hierdurch bewegt sich der Spieler Quer über den Bildschirm.
+             */
+
+
+            if(e.Key == Key.Left)
+            {
+                //Abfrage für nicht links?
+                hoch   = false;
+                runter = false;
+                rechts = false;
+
+                links  = true;
+
+                //PacMan drehen?
+                spieler.RenderTransform = new RotateTransform(-180);
+                aktivesspiel();
+
+            }
+
+            if (e.Key == Key.Right)
+            {
+                //Abfrage für nicht links?
+                hoch = false;
+                runter = false;
+                links = false;
+
+                rechts = true;
+
+                //PacMan drehen?
+                spieler.RenderTransform = new RotateTransform(180);
+                MapZeichnen();
+                aktivesspiel();
+
+            }
+
+            if (e.Key == Key.Up)
+            {
+                //Abfrage für nicht links?
+                rechts = false;
+                runter = false;
+                links = false;
+
+                hoch = true;
+
+                //PacMan drehen?
+                spieler.RenderTransform = new RotateTransform(180);
+                aktivesspiel();
+
+            }
+
+            if (e.Key == Key.Down)
+            {
+                //Abfrage für nicht links?
+                hoch = false;
+                rechts = false;
+                links = false;
+
+                runter = true;
+
+                //PacMan drehen?
+                spieler.RenderTransform = new RotateTransform(180);
+                aktivesspiel();
+
+            }
+
+            #endregion
+            
+        }
+        
+    }
+}
